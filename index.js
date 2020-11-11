b671c88eeb4f281adf9d732e1b53b2d6f1af8404
Justin Young <jyoung7223@yahoo.com>
2020-11-11 15:47:41 -0700

    Readme Generator file created

--- index.js
+++ index.js
@@ -1,108 +1,116 @@
-// array of questions for user
-const questions = [
-    "Enter Project Name:",
-    "Enter project Description:",
-    "Enter Installation Steps:",
-    "Enter Usage Infomration:",
-    "Enter Contribution Guidelines:",
-    "Enter Testing Steps:",
-    "Select License Type:",
-    "Enter GitHub Username:",
-    "Enter Contact Email:",
-];
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const genMD = require("./utils/generateMarkdown.js");
+const FILENAME = "./README.md";
 
 // array of possible license types
-const licenseTypes = [
-    "MIT",
-    "Apache 2.0",
-    "GPL 3.0",
-    "BSD 3",
-    "None"
-];
+const licenseTypes = new Map([
+    ['MIT', "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"],
+    ["Apache 2.0", "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"],
+    ["GPL 3.0", "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"],
+    ["BSD 3", "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"],
+    ["None", "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)"]
+]);
+
+// 2D Map array of questions for user in the format [ [ name, [type, message, choices]], ...]
+const questions = new Map([
+    ["title", ["input", "Enter Project Name:"]],
+    ["description", ["input", "Enter project Description:"]],
+    ["install", ["editor", "Enter Installation Steps (Save and Close editor when done):"]],
+    ["usage", ["editor", "Enter Usage Information (Save and Close editor when done):"]],
+    ["contribute", ["input", "Enter Contribution Guidelines:"]],
+    ["test", ["editor", "Enter Testing Command (Save and Close editor when done):"]],
+    ["github", ["input", "Enter GitHub Username:"]],
+    ["contact", ["input", "Enter Contact Email:"]],
+    ["licenseType", ["list", "Select License Type:", Array.from(licenseTypes.keys())]],
+    ["deployURL", ["input", "Enter URL to deployed Project:"]],
+    ["demoURL", ["input", "Enter URL to screenshot/gif of project:"]]
+]);
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(filename, data) {
+    fs.writeFile(filename, data, (err) => {
+        if (err) {
+            console.log(`Error:${err}`);
+        } else {
+            console.log(`Successfully generated ${filename}!`);
+        }
+    });
 }
 
 // function to initialize program
 function init() {
-
+    // Ask for title, description, licenseType[0], licenseType[1], github, contact, 
+    // deployURL, demoURL, install, usage, contribute, test
+    inquirer.prompt([
+        {
+            name: "title",
+            type: questions.get("title")[0],
+            message: questions.get("title")[1]
+        },
+        {
+            name: "description",
+            type: questions.get("description")[0],
+            message: questions.get("description")[1]
+        },
+        {
+            name: "licenseType",
+            type: questions.get("licenseType")[0],
+            message: questions.get("licenseType")[1],
+            choices: questions.get("licenseType")[2]
+        },
+        {
+            name: "contribute",
+            type: questions.get("contribute")[0],
+            message: questions.get("contribute")[1]
+        },
+        {
+            name: "github",
+            type: questions.get("github")[0],
+            message: questions.get("github")[1]
+        },
+        {
+            name: "contact",
+            type: questions.get("contact")[0],
+            message: questions.get("contact")[1]
+        },
+        {
+            name: "deployURL",
+            type: questions.get("deployURL")[0],
+            message: questions.get("deployURL")[1]
+        },
+        {
+            name: "demoURL",
+            type: questions.get("demoURL")[0],
+            message: questions.get("demoURL")[1]
+        },
+        {
+            name: "install",
+            type: questions.get("install")[0],
+            message: questions.get("install")[1]
+        },
+        {
+            name: "usage",
+            type: questions.get("usage")[0],
+            message: questions.get("usage")[1]
+        },
+        {
+            name: "test",
+            type: questions.get("test")[0],
+            message: questions.get("test")[1]
+        }
+    ]).then((data) => {
+        // Prepare response data before generating readme
+        data.licenseType = [data.licenseType, licenseTypes.get(data.licenseType)];
+        data.install = data.install.split("\n");
+        data.test = data.test.split("\n");
+        data.usage = data.usage.split("\n");
+        writeToFile(FILENAME, genMD.generateMarkdown(data));
+    }).catch((error) => {
+        console.log(`Error:${error}`)
+    });
 }
 
 // function call to initialize program
 init();
-
-
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-// name, location, bio, LinkedIn URL, and GitHub URL
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?',
-        },
-        {
-            type: 'input',
-            message: 'Where are you from?',
-            name: 'location',
-        },
-        {
-            type: 'input',
-            message: 'What is the URL for your bio?',
-            name: 'bio',
-        },
-        {
-            type: 'input',
-            message: 'What is the URL for your LinkedIn?',
-            name: 'linkedin',
-        },
-        {
-            type: 'input',
-            message: 'What is the URL for your GitHub?',
-            name: 'github',
-        },
-    ])
-    .then((data) => {
-        const { name, location, bio, linkedin, github } = data;
-        const filename = "index.html";
-        const htmlTemplate = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12 text-center bg-dark text-light">
-                    <h1>Hello my name is ${name}</h1>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12 text-center bg-light text-dark">
-                    <h3>I am from ${location}, learn more about me below:</h3>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-4 text-center bg-light text-dark">
-                    <p>Bio: ${bio}</p>
-                </div>
-                <div class="col-4 text-center bg-light text-dark">
-                    <p>LinkedIn: ${linkedin}</p>
-                </div>
-                <div class="col-4 text-center bg-light text-dark">
-                    <p>GitHub: ${github}</p>
-                </div>
-            </div>
-        </div>
-    </body>
-    </html>`;
-
-        fs.writeFile(filename, htmlTemplate, (err) => err ? console.log(err) : console.log('Success!'));
-    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,34 +1,62 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-// Project Name
-// License Type
-// Description
-// TOC
-// Install
-// Usage
-// License Desc
-// Contributing
-// Tests
-// Questions
-// Pic/Gif
   return `
 # ${data.title}
-[![${data.licenseType}](${data.licenseTypeImg} "${data.licenseType}")](${data.licenseTypeDetails})
+${data.licenseType[1]}
 ## Description
 ${data.description}
 ## Table of Contents
 * [\`Project\`](#)
 * [\`Description\`](#Description)
-* [\`Table of Contents\`]
+* [\`Table of Contents\`](#Table%20of%20Contents)
 * [\`1nstallation Steps\`](#Installation%20Steps)
-* [\`Usage Information\`] (#Usage Information)
-* [\`License\`] (#License)
-* [\`Contributing\`] (#Contributing)
-* [\`Testing\`] (#Testing)
-* [\`Questions\`] (#Questions)
-* [\`Sample\`] (#Sample)
-
+* [\`Usage Information\`](#Usage%20Information)
+* [\`Testing\`](#Testing)
+* [\`License\`](#License)
+* [\`Contributing\`](#Contributing)
+* [\`Questions\`](#Questions)
+* [\`Sample\`](#Sample)
+## Installation Steps
+To install this project, do the following:
+${printSteps(data.install)}
+## Usage Information
+To use this project, do the following:
+${printSteps(data.usage)}
+## Testing
+To test this project, do the following:
+${printSteps(data.test)}
+## License
+This project is provided under the ${data.licenseType[0]} license. For more information on license details click here ${data.licenseType[1]}
+## Contributing
+If you wish to contribute, do the following:
+${printSteps(data.contribute)}
+## Questions
+If you have any question about this repo, you may open an issue or contact me directly at ${data.contact}
+You can find more of my work at [My Github Repo](https://github.com/${data.github} "My GitHub Repo")
+## Sample
+Deployed project can be found at: ${data.deployURL}
+Below is a sample screenshot:
+![Project Sample](${data.demoURL} "Project Sample")
 `;
 }
 
-module.exports = generateMarkdown;
+// function to print an array in ordered list format
+function printSteps(steps){
+  let response = "";
+  if((steps == null)||(steps.length < 1)){
+    return;
+  }
+  
+  let type = typeof steps;
+  if(type === "string"){
+    return `> ${steps}`;
+  }
+
+  steps.forEach((step,index) => {
+    response += `> ${index}. ${step}`+"\n";
+  });
+  return response;
+}
+
+
+module.exports = {generateMarkdown};
